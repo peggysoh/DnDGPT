c8b67edf14775651e4328dc91e26f411d62ad808
Peggy Soh <peggysoh@microsoft.com>
2023-08-08 14:21:01 -0500

    add warning

--- dndbot/prompts/createCharacter.py
+++ dndbot/prompts/createCharacter.py
@@ -1,7 +1,7 @@
 prompt = '''Create a level 3 5e DnD character based off the DnD character sheet. 
 Use any available 5e DnD resources available to you. 
-Be diverse in your character creation. Have an even preference to all gender, race, and class options. 
-Pick from all available races and classes.
+Be diverse in your character creation. Have an even preference to all gender, race, and class. 
+Pick from all available races and classes. Do not have a preference for half-elves.
 Use language and tone of voice that fits the character.
 For backstory, keep it short but concised, it must be in 2-3 sentences. 
 For physical description, be descriptive but it must be in 2-3 sentences. 
